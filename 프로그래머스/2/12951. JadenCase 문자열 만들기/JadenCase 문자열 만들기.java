29ead98c29bc3ed05707b090d438e0172b9d8e55
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-06-20 13:33:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.07 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,39 +1,27 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s) {
         String answer = "";
-    	
-    	// " " 기준으로 문자열 잘라서 배열에 넣음
-    	String[] arr = s.split(" ");
-    	// 잘린 문자열을 순서대로 처리
-    	for(int i=0; i<arr.length; i++) {
-    		String now = arr[i];
-    		
-    		// 문자열의 길이가 0이라면(공백 이라면)
-    		// answer에 " "만 추가
-    		if(arr[i].length() == 0) {
-    			answer += " ";
-    		} 
-    		// 문자가 있다면
-    		else {
-    			// 0번째 문자는 대문자로
-    			answer += now.substring(0, 1).toUpperCase();
-    			// 1번째 문자부터 마지막까지는 소문자로
-    			answer += now.substring(1, now.length()).toLowerCase();
-    			// 마지막에 " " 추가
-    			answer += " ";
-    		}
-    		
-    	}
-    	
-    	// 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
-    	if(s.substring(s.length()-1, s.length()).equals(" ")){
-    		return answer;
-    	}
-    	
-    	// 맨 마지막 " " 제거하고 answer 반환
+        
+        // 문자열을 띄어쓰기 단위로 문자로 나눠 배열에 저장
+        String[] str = s.split(" ");
+        
+        // 각 배열의 첫번째 글자가 숫자가 아니라면 대문자로 변경, 나머지 문자는 소문자로 변경
+        for(int i=0; i<str.length; i++) {
+            String now = str[i];
+            if(str[i].length() == 0) {
+                answer += " ";
+            } else {
+            answer += now.substring(0,1).toUpperCase();
+            answer += now.substring(1,now.length()).toLowerCase();
+            answer += " ";                
+            }
+
+        }
+        
+        if(s.substring(s.length()-1, s.length()).equals(" ")) {
+            return answer;
+        }
+        
         return answer.substring(0, answer.length()-1);
     }
-
 }
