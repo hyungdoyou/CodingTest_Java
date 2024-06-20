6b3cf3209f952ceaf135cb35b5666033c1182004
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-06-20 14:01:53 +0900

    [level 2] Title: 타겟 넘버, Time: 0.44 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,19 @@
-import java.util.*;
-
 class Solution {
+    int answer = 0;
+
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        
-        Queue<Integer> queue = new LinkedList<>();
-        
-        Integer sum = 0;
-        queue.add(0);
-        
-        for(int i=0; i<numbers.length; i++) {
-            for(int j=0; j<queue.size(); j++) {
-                sum = queue.poll();
-                queue.add(sum + numbers[i]);
-                queue.add(sum - numbers[i]);   
-            }
-        }
-        
-        while(!queue.isEmpty()){
-            Integer result = queue.poll();
-            if(result == target) {
-                answer = answer+1;
-            }
-        }
+        dfs(numbers, 0, target, 0);
+
         return answer;
+    }
+
+    // 깊이 우선 탐색
+    public void dfs(int[] numbers, int depth, int target, int sum){
+        if(depth == numbers.length){ // 마지막 노드 까지 탐색한 경우
+            if(target == sum) answer++;
+        } else {
+            dfs(numbers, depth + 1, target, sum + numbers[depth]); // 해당 노드의 값을 더하고 다음 깊이 탐색
+            dfs(numbers, depth + 1, target, sum - numbers[depth]); // 해당 노드의 값을 빼고 다음 깊이 탐색
+        }
     }
 }
