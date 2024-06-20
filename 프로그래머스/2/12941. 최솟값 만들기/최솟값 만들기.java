671ac07c7ebc29c0bbd53c2796cee41c5a0f43d4
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-06-20 13:17:41 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.80 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,16 +1,19 @@
 import java.util.*;
 class Solution
 {
-   	 public static int solution(int []A, int []B)
-		    {
-		        int answer = 0;
-		        Arrays.sort(A);
-		        Arrays.sort(B);
-		        for(int i=0;i<A.length;i++) {
-		        	
-		        		answer=answer+(A[i]*B[A.length-i-1]);
-		        	
-		        }
-		        return answer;
-		    }
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        
+        Arrays.sort(A); // A 배열을 오름차순으로 정렬
+        Arrays.sort(B); // B 배열을 오름차순으로 정렬
+        
+        // 누적이 최소가 되는 값 = A 배열 오름차순 * B 배열 내림차순
+        // A와 B의 길이는 같음
+        for(int i=0; i<A.length; i++) {
+            answer += A[i] * B[B.length-1-i];
+        }
+
+        return answer;
+    }
 }
