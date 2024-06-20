491fe20e07c9cd997ae2e3a863ed54604ce868a7
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-06-20 16:12:27 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,33 @@
-import java.util.*;
-
 class Solution {
-    public void search(int[][] lists, boolean[] visited, Integer startNum) {
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(startNum);
-        visited[startNum] = true;
-
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-
-            int[] neighbors = lists[node];
-            for (int i = 0; i < neighbors.length; i++) {
-                if (!visited[i]) {
-                    if (neighbors[i] == 1) {
-                        queue.add(i);
-                        visited[i] = true;
-                    }
-                }
-            }
-        }
-    }
-
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[computers.length];
-        for (int i = 0; i < computers.length; i++) {
-            if (visited[i] == false) {
-                answer = answer + 1;
-                search(computers, visited, i);
-            }
+        
+        // 방문했는지 여부를 확인하기 위한 배열 선언
+        Boolean visited[] = new Boolean[computers.length];
+        
+        // 처음에는 모두 방문 안한 것으로 초기화
+        for(int i=0; i<visited.length; i++) {
+            visited[i] = false;
         }
 
+        // dfs 구현
+        for(int i=0; i<computers.length; i++) {
+            if(visited[i] == false) { // 아직 방문전이라면
+                answer++;
+                dfs(i, visited, computers);                
+            }
+        }
+                
         return answer;
+    }
+    
+    public void dfs(int i, Boolean[] visited, int[][] computers) {
+        visited[i] = true; // 현재 노드는 방문으로 변경
+        
+        for(int j=0; j<computers.length; j++) {
+            if(visited[j]==false && computers[i][j]==1) {
+                dfs(j, visited, computers);
+            }
+        }
     }
 }
