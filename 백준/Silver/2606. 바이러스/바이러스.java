7eef7c016271ec25af3a766a66c9670b24d23606
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-06-28 16:00:35 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M, answer;
+    
+    public static void dfs(int idx) {
+        if(visited[idx] == true) {
+            return;
+        }
+        
+        for(int i=0; i<graph[idx].size(); i++) {
+            visited[idx] = true;   
+
+            int next = graph[idx].get(i);
+            if(visited[next] == false) {
+               dfs(next);   
+            }
+        }
+        answer++;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        N = Integer.parseInt(br.readLine()); // 노드의 수
+        M = Integer.parseInt(br.readLine()); // 간선의 수
+        
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N+1];
+        
+        // 각 노드별 graph 객체 생성
+        for(int i=1; i<=N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        int x, y;
+        
+        // 간선 정보를 입력받아 그래프를 구성
+        for(int i=0; i<M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            graph[y].add(x);
+            graph[x].add(y);
+        }
+        
+        dfs(1);
+        
+        bw.write(String.valueOf(answer - 1));
+        
+        bw.close();
+        br.close();
+        
+    }
+}
