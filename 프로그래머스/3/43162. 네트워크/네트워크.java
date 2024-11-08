eadccfb02bfb1ed3740a67685730820171990d90
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-11-08 15:30:33 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,34 @@
-class Solution {
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        
-        // 방문했는지 여부를 확인하기 위한 배열 선언
-        Boolean visited[] = new Boolean[computers.length];
-        
-        // 처음에는 모두 방문 안한 것으로 초기화
-        for(int i=0; i<visited.length; i++) {
-            visited[i] = false;
-        }
+import java.util.*;
 
-        // dfs 구현
-        for(int i=0; i<computers.length; i++) {
-            if(visited[i] == false) { // 아직 방문전이라면
+class Solution {
+    
+    static int[][] computers;
+    static boolean[] visited;
+    static int n;
+    static int answer = 0;
+    
+    public int solution(int n, int[][] computers) {
+        
+        this.n = n;
+        this.computers = computers;
+        
+        visited = new boolean[n+1];
+        
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == false) {
+                dfs(i);
                 answer++;
-                dfs(i, visited, computers);                
             }
         }
-                
+        
         return answer;
     }
     
-    public void dfs(int i, Boolean[] visited, int[][] computers) {
-        visited[i] = true; // 현재 노드는 방문으로 변경
-        
-        for(int j=0; j<computers.length; j++) {
-            if(visited[j]==false && computers[i][j]==1) {
-                dfs(j, visited, computers);
+    public void dfs(int idx) {
+        visited[idx] = true;
+        for(int i=0; i<n; i++) {
+            if(visited[i] == false && computers[idx][i] == 1) {
+                dfs(i);
             }
         }
     }
