13f29edfade349132ac3ca941202b99a9fdf13d2
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-11-08 16:12:05 +0900

    [level 3] Title: 여행경로, Time: 154.24 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,37 +1,37 @@
 import java.util.*;
 
 class Solution {
-    boolean[] visited; // 방문한지 안한지를 체크하는 visited 배열
+    
+    boolean[] visited;  // 방문여부
     List<String> result = new ArrayList<>();
-
+    
     public String[] solution(String[][] tickets) {
-        // 1. 변수 세팅
-        visited = new boolean[tickets.length];
+        String[] answer = {};
         
-        // 2. 완전탐색 및 정렬
+        visited = new boolean[tickets.length];  // 초기화
+        
+        // 완전 탐색 시장
         dfs(0, "ICN", "ICN", tickets);
+        
         Collections.sort(result);
         
-        // 3. 정답 출력
-        String[] answer = result.get(0).split(" ");
+        answer = result.get(0).split(" ");    
         return answer;
     }
-
+    
     public void dfs(int idx, String start, String route, String[][] tickets) {
-        // 1. 탈출 조건 (비행기 티켓을 모두 사용한 경우)
-        if (idx == tickets.length) {
+        // 탈출 조건
+        if(idx == tickets.length) {
             result.add(route);
             return;
         }
-
-        // 2. 백트래킹 알고리즘 사용
-        for (int i = 0; i < tickets.length; i++) {
-            if (tickets[i][0].equals(start) && visited[i] == false) {
+        
+        for(int i = 0; i < tickets.length; i++) {
+            if(tickets[i][0].equals(start) && visited[i] == false) {
                 visited[i] = true;
-                dfs(idx + 1, tickets[i][1], route + " " + tickets[i][1], tickets);
+                dfs(idx+1, tickets[i][1], route + " " + tickets[i][1], tickets);
                 visited[i] = false;
             }
         }
-        return;
     }
 }
