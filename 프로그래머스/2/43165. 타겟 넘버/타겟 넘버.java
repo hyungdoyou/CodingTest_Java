886628162417d75eb0aee7cf561dd6f730c0ba09
hyungdoyou <148875644+hyungdoyou@users.noreply.github.com>
2024-11-08 10:17:34 +0900

    [level 2] Title: 타겟 넘버, Time: 7.14 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,32 +1,32 @@
+import java.util.*;
+
 class Solution {
-    int answer;
-    int target;
-    int[] numbers;
-
-        // 깊이 우선 탐색
+    
+    static int answer = 0;
+    static int target;
+    static int[] numbers;
+    
     public void dfs(int index, int sum) {
-        // 탈출 조건 : 마지막 노드까지 탐색한 경우
-        if (index == numbers.length) {
-            if (target == sum) {
-                answer++; // 타겟 숫자와 합계가 같으면 방법의 수 증가
+        // 1. 탈출 조건
+        if(index == numbers.length) {
+            if(sum == target) {
+                answer++;
             }
-            return; // 탐색 종료
+            return;
         }
-
-        // 수행동작
-        // 해당 노드의 값을 더하고 다음 깊이 탐색
+        
+        // 2. 수행 동작
         dfs(index + 1, sum + numbers[index]);
-        // 해당 노드의 값을 빼고 다음 깊이 탐색
         dfs(index + 1, sum - numbers[index]);
     }
-    
-    
     public int solution(int[] numbers, int target) {
-        answer = 0;
+        
         this.numbers = numbers;
         this.target = target;
         
-        dfs(0, 0); // DFS 탐색 시작
-        return answer; // 최종 결과 반환
+        dfs(0, 0);
+        
+        
+        return answer;
     }
 }
